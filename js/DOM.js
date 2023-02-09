853590c4c7e784ebe0e55f12b7f0fbca63ae6c7a
sobuj <sobujahmed124@gmail.com>
2023-02-09 19:26:03 +0600

    tour of dom

---
+++ js/DOM.js
@@ -0,0 +1,6 @@
+// all the js output will be shown in console
+console.log("this is dom");
+
+// document gives object of html in console
+console.log(document);
+// we can see whole html inside this document
